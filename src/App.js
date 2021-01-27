2c1333005e8589f80fcc36fe8398bb24733dfed0
vensky <ivan.fedotovskikh@gmail.com>
2021-01-27 22:08:34 +0500

    Добавляет компонент Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './components/Header/Header.js';
+import Layout from './components/Layout/Layout.js';
 
 function App() {
   return (
     <>
         <Header />
+        <Layout />
+        <Layout />
+        <Layout />
     </>
   );
 }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,25 @@
+import s from './style.module.css';
+
+const Layout = (id, title, descr, urlBg, colorBg) => {
+    return (
+        <section class="root" id="">
+            <div class="wrapper">
+                <article>
+                    <div class="title">
+                        <h3>
+
+                        </h3>
+                        <span class="separator"></span>
+                    </div>
+                    <div class="desc full">
+                        <p>
+
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Layout;
